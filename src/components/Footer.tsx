f8d969e293a59328c316b598712fb1205f69150a
Michael Peter <michaelpeter.ai@gmail.com>
2022-10-28 17:20:47 +0100

    footer looking good

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,78 @@
-import { Grid, styled } from '@mui/material';
+import { Grid, styled, Typography } from '@mui/material';
+import zuriImage from '../images/zuri.png';
+import i4gImage from '../images/i4g.png';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <Wrapper>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={4}></Grid>
+        <GridItem xs={12} md={4}>
+          <ZuriWrapper>
+            <Image
+              src={zuriImage}
+              alt='Zuri Internship'
+              layout='fill'
+              objectFit='cover'
+            />
+          </ZuriWrapper>
+        </GridItem>
+
+        <GridItem xs={12} md={4}>
+          <Text>HNG Internship 9 Frontend Task</Text>
+        </GridItem>
+
+        <GridItem xs={12} md={4}>
+          <I4gWrapper>
+            <Image
+              src={i4gImage}
+              alt='Ingressive for Good'
+              layout='fill'
+              objectFit='cover'
+            />
+          </I4gWrapper>
+        </GridItem>
       </Grid>
     </Wrapper>
   );
 }
 
 const Wrapper = styled('footer')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
+  paddingTop: 48,
+  paddingBottom: 48,
+  borderTop: '1.5px Solid rgba(0, 0, 0, 0.1)',
+
+  [theme.breakpoints.down('sm')]: {
+    paddingBottom: 32,
+  },
 }));
+
+const GridItem = styled((props: any) => <Grid item {...props} />)(
+  ({ theme }) => ({
+    display: 'flex',
+    alignItems: 'flex-end',
+
+    [theme.breakpoints.up('sm')]: {
+      justifyContent: 'center',
+    },
+  })
+);
+
+const Text = styled(Typography)({
+  fontSize: 16,
+  fontWeight: 400,
+  lineHeight: '24px',
+  textAlign: 'center',
+});
 
 const ZuriWrapper = styled('div')({
   position: 'relative',
-  height: '24.73px',
   width: '185.03px',
+  height: '32px',
+});
+
+const I4gWrapper = styled('div')({
+  position: 'relative',
+  width: '132.13px',
+  height: '32px',
 });
